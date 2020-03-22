d4442308b732dac8ee144e9b487b246bd4904eea
Banuraga <andika_w@hotmail.com>
2020-03-22 14:06:51 +0900

    Initial Commit

---
+++ Say No to Forget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Say No to Forget
+//
+//  Created by Andika Banuraga on 22/03/20.
+//  Copyright © 2020 Andika Banuraga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
